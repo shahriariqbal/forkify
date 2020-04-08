edc257ae2010949dfdc5eb495eec3e46d1ec0c0f
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-08 14:39:17 +0600

    Making First API Calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,33 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+//*****Import Export Module basic concept*******//
+
+//import str from './models/Search';
+
+//import {add as a, multiply as m, ID} from './views/searchView';
+//console.log(`Using imported functions! ${a(ID,2)} and ${m(3, 5)}. ${str}`);
+
+//another way
+//import * as searchView from './views/searchView';
+//console.log(`Using imported functions! ${searchView.add(searchView.ID,2)} and ${searchView.multiply(3, 5)}. ${str}`);
+
+//*********************************************Project Start**************************************************************************//
+
+//https://forkify-api.herokuapp.com/api/search
+//https://forkify-api.herokuapp.com/api/search?q=pizza
+//https://crossorigin.me/
+//https://cors-anywhere.herokuapp.com/
+
+import axios from 'axios';
+
+async function getResults(query){
+	const proxy = 'https://cors-anywhere.herokuapp.com/';
+	try{
+		const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?q=${query}`);
+	    const recipes = res.data.recipes;
+	    console.log(recipes);
+	}catch(error){
+		alert(error);
+	}
+
+	
+}
+getResults('carrot');
