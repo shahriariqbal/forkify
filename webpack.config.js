f81e7ccf265c15ac73cfe7c305a3561d1d642591
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-11 00:26:50 +0600

    Fresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: ['./src/js/index.js'],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename:'js/bundle.js'
-	},
-	devServer: {
-		contentBase: './dist'
-	},
-	plugins: [
-	new HtmlWebpackPlugin({
-		filename: 'index.html',
-		template: './src/index.html'
-	})
-	],
-	module: {
-		rules:[
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader'
-			}
-		}
-		]
-	}
-	
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
