16499444d48672dac74ed83122a4844ef5b21bde
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-10 13:57:30 +0600

    Building the Search View update2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,19 +5,34 @@
 // export const ID = 23;
 
 //*********************************************Project Start**************************************************************************//
-import {elements} from './base';
+import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
 
-export const clearInput = () =>{
-    elements.searchInput.value = '';	
-} ;
+export const clearInput = () => {
+	elements.searchInput.value = '';
+};
 
 export const clearResults = () => {
 	elements.searchResList.innerHTML = '';
 };
 
-const renderRecipe = recipe => {
+const limitRecipeTitle = (title, limit = 17) => {
+	const newTitle = [];
+	if (title.length > limit) {
+		title.split(' ').reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur);
+			}
+			return acc + cur.length;
+		}, 0);
+		//return the result
+		return `${newTitle.join(' ')} ...`;
+	}
+	return title;
+};
+
+const renderRecipe = (recipe) => {
 	const markup = `
       <li>
     <a class="results__link " href="#${recipe.recipe_id}">
@@ -25,7 +40,7 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title) }</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
@@ -34,6 +49,6 @@ const renderRecipe = recipe => {
 	elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
+export const renderResults = (recipes) => {
 	recipes.forEach(renderRecipe);
 };
