bdb4513f552c83df361d579f57aa2d0c02d387f0
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-10 23:08:34 +0600

    revert to update 2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,34 +5,19 @@
 // export const ID = 23;
 
 //*********************************************Project Start**************************************************************************//
-import { elements } from './base';
+import {elements} from './base';
 
 export const getInput = () => elements.searchInput.value;
 
-export const clearInput = () => {
-	elements.searchInput.value = '';
-};
+export const clearInput = () =>{
+    elements.searchInput.value = '';	
+} ;
 
 export const clearResults = () => {
 	elements.searchResList.innerHTML = '';
 };
 
-const limitRecipeTitle = (title, limit = 17) => {
-	const newTitle = [];
-	if (title.length > limit) {
-		title.split(' ').reduce((acc, cur) => {
-			if (acc + cur.length <= limit) {
-				newTitle.push(cur);
-			}
-			return acc + cur.length;
-		}, 0);
-		//return the result
-		return `${newTitle.join(' ')} ...`;
-	}
-	return title;
-};
-
-const renderRecipe = (recipe) => {
+const renderRecipe = recipe => {
 	const markup = `
       <li>
     <a class="results__link " href="#${recipe.recipe_id}">
@@ -40,7 +25,11 @@ const renderRecipe = (recipe) => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
+<<<<<<< HEAD
             <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+=======
+            <h4 class="results__name">${recipe.title}</h4>
+>>>>>>> parent of 1649944... Building the Search View update2
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
@@ -49,6 +38,7 @@ const renderRecipe = (recipe) => {
 	elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
+<<<<<<< HEAD
 const createButton = (page, type) => `
 
         <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
@@ -89,3 +79,8 @@ export const renderResults = (recipes, page = 2, resPerPage = 10) => {
 	//render pagination buttons
 	renderButtons(page, recipes.length, resPerPage);
 };
+=======
+export const renderResults = recipes => {
+	recipes.forEach(renderRecipe);
+};
+>>>>>>> parent of 1649944... Building the Search View update2
