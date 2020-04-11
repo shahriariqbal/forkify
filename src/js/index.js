60f948dcbbb92ace33771dc31f29906ac107da0b
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-11 22:42:22 +0600

    Building the Recipe View -update2

--- src/js/index.js
+++ src/js/index.js
@@ -111,6 +111,9 @@ const controlRecipe = async () => {
 	   //Prepare UI for changes
 	   recipeView.clearRecipe();
 	   renderLoader(elements.recipe);
+
+	   //Highlight selected search item
+	   if(state.search)  searchView.highlightSelected(id);
 	   
 	   //Create new Recipe object
 		 state.recipe = new Recipe(id);
