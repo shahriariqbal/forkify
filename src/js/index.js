418bea83c0c1646ab6cb3db966e7b912930b5fe0
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-12 17:41:45 +0600

    Building the Likes Controller

--- src/js/index.js
+++ src/js/index.js
@@ -36,10 +36,12 @@ getResults('carrot');*/
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
 import {elements, renderLoader, clearLoader} from './views/base';
+
 
 
 
@@ -185,6 +187,43 @@ elements.shopping.addEventListener('click', e => {
 
 });
 
+
+//*************************Like Controller********************************************* */
+const controlLike = () => {
+	  if(!state.likes) state.likes = new Likes();
+	  const currentID = state.recipe.id;
+
+
+	  //User has not yet liked current recipe
+	  if(!state.likes.isLiked(currentID)){
+		  //Add like to the state
+		  const newLike = state.likes.addLike(
+			  currentID,
+			  state.recipe.title,
+			  state.recipe.author,
+			  state.recipe.img
+		  );
+
+		  //Toggle the like button
+
+		  //Add like to UI list
+		  console.log(state.likes);
+		  
+		  //User has  liked current recipe
+	  }else{
+		  //Remove like from the state
+		  state.likes.deleteLike(currentID);
+
+		  //Toggle the like button
+
+		  //Remove like from UI list
+		  console.log(state.likes);
+
+	  }
+};
+
+
+
 //Handling recipe button clicks
 
 elements.recipe.addEventListener('click', e => {
@@ -205,6 +244,9 @@ elements.recipe.addEventListener('click', e => {
 		recipeView.updateServingsIngredients(state.recipe);
 	}else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
 		controlList();
+
+	}else if(e.target.matches('.recipe__love, .recipe__love *')){
+		controlLike();
 
 	}
 	
